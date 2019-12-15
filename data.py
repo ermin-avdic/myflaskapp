d8b725a6ed4f067111b7e3b676e36a90c3f94128
Ermin <usb007agent@gmail.com>
2019-12-15 23:08:08 +0100

    Flask app project

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum doloremque earum error, ducimus itaque, quaerat et iure consequuntur facilis natus optio eligendi culpa suscipit, rerum totam. Quidem doloremque sapiente quod?',
+            'author': 'Ermin Avdic',
+            'create_date': '15-12-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum doloremque earum error, ducimus itaque, quaerat et iure consequuntur facilis natus optio eligendi culpa suscipit, rerum totam. Quidem doloremque sapiente quod?',
+            'author': 'Brad Traversy',
+            'create_date': '15-12-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum doloremque earum error, ducimus itaque, quaerat et iure consequuntur facilis natus optio eligendi culpa suscipit, rerum totam. Quidem doloremque sapiente quod?',
+            'author': 'Ermin Avdic',
+            'create_date': '15-12-2017'
+        }
+    ]
+    return articles
