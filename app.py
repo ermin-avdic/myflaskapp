9f8173673c60d611db4cfa203f0d9aaf5fd89eb7
Ermin <usb007agent@gmail.com>
2019-12-16 02:42:45 +0100

    Added regsiter functionality

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, request, url_for, session, logging
+from flask_sqlalchemy import SQLAlchemy
 from data import Articles
+from datetime import datetime
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+import psycopg2
 
 app = Flask(__name__)
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:toor@localhost/myflaskapp'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class UserInfo(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    email= db.Column(db.String(200))
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    register_date = db.Column(db.DateTime, default='datetime.now')
+
+    def __init__(self, name, email, username, password):
+        self.name = name
+        self.email = email
+        self.username = username
+        self.password = password
 
 Articles = Articles()
 
@@ -21,5 +54,41 @@ def articles():
 def article(id):
     return render_template('article.html', id=id)
 
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match.')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        con = psycopg2.connect("dbname=myflaskapp user=postgres password=toor")
+        cur = con.cursor()
+
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        con.commit()
+
+        cur.close()
+
+        flash('You are now registerd and can log in', 'success')
+
+        return redirect(url_for('index'))
+
+    return render_template('register.html', form=form)
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.secret_key = 'secret123'
+    app.run()
